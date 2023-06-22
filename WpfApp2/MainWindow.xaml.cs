2d46e34a5ed0410d4c25b144c4b8df2e7d39d36b
Camal_pz37 <Camal_pz37@STHQ012B-06>
2023-06-22 13:48:14 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Image image = (Image)button.Content;
+            statusBarImage.Source = image.Source;
+        }
+    }
+}
